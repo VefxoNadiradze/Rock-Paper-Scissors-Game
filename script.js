bc5370ea4864e331466869e2e61ebbc08f1c59a3
VefxoNadiradze <olld00@bk.ru>
2025-01-11 19:12:25 +0400

    import all important elements in js from html

---
+++ script.js
@@ -0,0 +1,6 @@
+const playerScoreEl = document.querySelector(".playerScore");
+const computerScoreEl = document.querySelector(".computerScore");
+const optionBtns = Array.from(document.querySelectorAll(".optionBtn"));
+const playerImg = document.querySelector(".playerImg");
+const computerImg = document.querySelector(".computerImg");
+const winnerEl = document.querySelector(".winner");
