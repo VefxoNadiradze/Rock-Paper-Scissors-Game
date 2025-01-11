5b1045aec9670c3e85992dc940e53ce249de12d7
VefxoNadiradze <olld00@bk.ru>
2025-01-11 19:33:09 +0400

    initial all important functions for the game

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const optionBtns = Array.from(document.querySelectorAll(".optionBtn"));
 const playerImg = document.querySelector(".playerImg");
 const computerImg = document.querySelector(".computerImg");
 const winnerEl = document.querySelector(".winner");
+
+initianGame();
+function initianGame() {}
+
+function clickOptions() {}
+
+function winnerFunction() {}
