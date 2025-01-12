7ba3a4b0831ebec1b88183fe0adeeba1c134fd57
VefxoNadiradze <olld00@bk.ru>
2025-01-12 14:09:06 +0400

    change the image of player on the click and random for the computer

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const winnerEl = document.querySelector(".winner");
 let playerScore = 0;
 let computerScore = 0;
 
+const images = [
+  "./assets/rock.png",
+  "./assets/paper.png",
+  "./assets/scissors.png",
+];
+
 initianGame();
 function initianGame() {
   optionBtns.map((option) => option.addEventListener("click", clickOptions));
@@ -21,6 +27,22 @@ function clickOptions() {
     return;
   }
   let clickedOption = this.classList.add("clickedOption");
+
+  switch (this.innerText) {
+    case "Rock":
+      playerImg.src = images[0];
+      break;
+    case "Paper":
+      playerImg.src = images[1];
+      break;
+
+    case "Scissor":
+      playerImg.src = images[2];
+      break;
+  }
+  let computerImgIndex = Math.floor(Math.random() * images.length);
+
+  computerImg.src = images[computerImgIndex];
 
   changePlayer(clickedOption);
 }
