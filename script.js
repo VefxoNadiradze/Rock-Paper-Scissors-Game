b86cec0019c2dd4b91a59389101d339e7624ba06
VefxoNadiradze <olld00@bk.ru>
2025-01-12 13:42:30 +0400

    add class on the clicled option btn and calling the changeplayer function

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-const playerScoreEl = document.querySelector(".playerScore");
-const computerScoreEl = document.querySelector(".computerScore");
+const playerScoreEl = document.querySelector(".playerScore span");
+const computerScoreEl = document.querySelector(".computerScore span");
 const optionBtns = Array.from(document.querySelectorAll(".optionBtn"));
 const playerImg = document.querySelector(".playerImg");
 const computerImg = document.querySelector(".computerImg");
 const winnerEl = document.querySelector(".winner");
+let playerScore = 0;
+let computerScore = 0;
 
 initianGame();
-function initianGame() {}
+function initianGame() {
+  optionBtns.map((option) => option.addEventListener("click", clickOptions));
+  playerScoreEl.innerText = playerScore;
+  computerScoreEl.innerText = computerScore;
+}
 
-function clickOptions() {}
+function clickOptions() {
+  if (
+    [...optionBtns].some((button) => button.classList.contains("clickedOption"))
+  ) {
+    return;
+  }
+  let clickedOption = this.classList.add("clickedOption");
+
+  changePlayer(clickedOption);
+}
+
+function changePlayer(clickedOption) {}
 
 function winnerFunction() {}
